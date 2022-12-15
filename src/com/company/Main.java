9d6498c667bfb99e0c5ed032e21f7d461e94a33c
Denys Stefanko <denys.stefanko@vitechteam.com>
2022-12-15 12:32:06 +0200

    renamed variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ import com.company.factories.AbstractFactory;
 import com.company.factories.FactoryProvider;
 import com.company.factories.FactoryType;
 import com.company.factories.animal.Animal;
-import com.company.factories.vehicles.Car;
 import com.company.factories.vehicles.Vehicle;
 
 public class Main {
@@ -13,12 +12,12 @@ public class Main {
         AbstractFactory<?> abstractFactory;
 
         abstractFactory = FactoryProvider.getFactory(FactoryType.ANIMAL);
-        Animal dog = (Animal) abstractFactory.create("Cat");
+        Animal animal = (Animal) abstractFactory.create("Dog");
 
         abstractFactory = FactoryProvider.getFactory(FactoryType.VEHICLE);
-        Vehicle car = (Vehicle) abstractFactory.create("Truck");
+        Vehicle vehicle = (Vehicle) abstractFactory.create("Truck");
 
-        dog.makeSound();
-        car.getType();
+        animal.makeSound();
+        vehicle.getType();
     }
 }
